e5c72709abe33be41d3fa96f188ea540e2174b01
Joshua Hegedus <josh.hegedus@outlook.com>
2022-04-15 15:10:42 +0200

    Rot scripts added

---
+++ rot13.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+translate () {
+	echo -n  "$*" | tr 'A-Za-z' 'N-ZA-Mn-za-m'
+}
+
+main () {
+	read -r input
+	
+	if [ "$input" == "" ]; then
+		input="$(xsel -b )"
+	fi
+
+	result="$(translate $input )"
+	echo $result
+	echo -n $result | xsel -b -i
+}
+
+while [ 3 == 3 ]; do 
+	main
+done
